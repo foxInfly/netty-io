56edfccade6ec621ce617da2abb97728cff119a5
puli <123@123.com>
2020-08-28 22:06:42 +0800

    nio-buffer内存映射

---
+++ src/main/java/com/pupu/io/nio/buffer/MappedBuffer.java
@@ -0,0 +1,31 @@
+package com.pupu.io.nio.buffer;
+
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * @author : lipu
+ * @since : 2020-08-28 21:47
+ */
+public class MappedBuffer {
+    
+    private static final int start = 0;
+    private static final int size = 1024;
+    
+    
+    public static void main(String[] args) throws Exception {
+        RandomAccessFile raf = new RandomAccessFile("E://test.txt", "rw");
+        FileChannel fc = raf.getChannel();
+
+        //把缓冲区跟文件系统进行一个映射关联
+        //只要操作缓冲区里面的内容，文件内容也会跟着改变
+        MappedByteBuffer mbb = fc.map(FileChannel.MapMode.READ_WRITE, start, size);
+
+        mbb.put(0, (byte) 97);
+        mbb.put(1023, (byte) 122);
+
+        raf.close();
+    }
+}
